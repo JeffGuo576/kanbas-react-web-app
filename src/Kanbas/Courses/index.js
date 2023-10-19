dd69f346f3a94e476a1009cf347d8036b46c656f
JeffGuo576 <jeffreyguo315@gmail.com>
2023-10-19 03:54:12 -0400

    Fixed more problems

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -1,5 +1,5 @@
 import db from "../../Kanbas/Database";
-import { Navigate, Route, Routes, useParams, useLocation} from "react-router-dom";
+import { Route, Routes, useParams, useLocation} from "react-router-dom";
 import CourseNavigation from "./CourseNavigation";
 import Modules from "./Modules";
 import Home from "./Home";
@@ -7,7 +7,7 @@ import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/AssignmentEditor";
 import Grades from "./Grades";
 import '../index.css';
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faAngleDown} from "@fortawesome/free-solid-svg-icons";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -15,7 +15,7 @@ import {
 } from '@chakra-ui/react';
 import { ChevronRightIcon } from '@chakra-ui/icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXmark, faGauge, faBook, faCalendarDays, faInbox, faClock, faVideo, faArrowRightFromBracket, faCircleQuestion } from "@fortawesome/free-solid-svg-icons";
+import { faGauge, faBook, faCalendarDays, faInbox, faClock, faVideo, faArrowRightFromBracket, faCircleQuestion } from "@fortawesome/free-solid-svg-icons";
 
 function Courses() {
   const { courseId } = useParams();
@@ -44,17 +44,52 @@ function Courses() {
             <hr/>
             </td>
             <td className="md-dropdown d-md-table-cell d-xl-none d-lg-none">
-                <div className="d-flex justify-content-between">
-                    <a className="btn btn-black d-flex justify-content-between align-items-center" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <FontAwesomeIcon icon={faBars} color="white"></FontAwesomeIcon>
+            <div className="d-flex justify-content-between" style={{ backgroundColor: 'black' }}>
+                <a
+                  className="btn btn-black d-flex justify-content-between align-items-center"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                  style={{ width: '45px', color: 'white' }}
+                >
+                  <FontAwesomeIcon icon={faBars} style={{ color: '#ee3a3a' }} />
+                </a>
+                <ul className="kanbas dropdown-menu" style={{ width: '100%' }}>
+                  <li className="dropdown-item active">
+                    <button style={{ border: 'none', backgroundColor: 'white' }}>
+                      <FontAwesomeIcon icon="x" className="fa-solid" />
+                    </button>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="/Kanbas/Dashboard">
+                      <FontAwesomeIcon icon="gauge" className="fa-solid" style={{ color: 'red' }} />
+                      Dashboard
                     </a>
-                    <ul className="dropdown-menu">
-                      <li><a className="dropdown-item text-end" href="/"></a><FontAwesomeIcon icon={faXmark}></FontAwesomeIcon></li>
-                      {dropDownItems.map((item, index) => (
-                        <li><a className="dropdown-item" href="#"><FontAwesomeIcon icon={item}></FontAwesomeIcon></a>{dropDownItemNames[index]}</li>
-                      ))}
-                    </ul>
+                  </li>
+                </ul>
+                <div className="breadcrumbforkanbas" style={{ color: 'white' }}>
+                  CS4550.12631.202410 <br />
+                  Home
                 </div>
+                <a
+                  className="btn btn-black d-flex justify-content-between align-items-center"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                  style={{ width: '45px', color: 'white' }}
+                >
+                  <FontAwesomeIcon icon={faAngleDown} style={{ color: '#ffffff' }} />
+                </a>
+                <ul className="course dropdown-menu" style={{ width: '100%' }}>
+                  <li className="dropdown-item active">
+                    <button style={{ border: 'none', backgroundColor: 'white' }}>
+                      <FontAwesomeIcon icon="x" className="fa-solid" />
+                    </button>
+                  </li>
+        </ul>
+      </div>
             </td>
           </tr>
           <tr>
